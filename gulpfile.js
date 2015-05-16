f16f6fea464216c12eaa14770f6f1229d0e38dc9
cuboktahedron <cuboktahedron@gmail.com>
2015-05-16 21:04:53 +0900

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,51 @@
+var gulp = require('gulp');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var sourcemaps = require('gulp-sourcemaps');
+var buffer = require('vinyl-buffer');
+var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat')
+var minifyCss = require('gulp-minify-css')
+var rename = require('gulp-rename')
+//var glob = require('glob');
+var plumber = require('gulp-plumber');
+
+gulp.task('browserify', function () {
+//  var scriptFiles = glob.sync('./scripts/**/script.js');
+
+  return browserify({
+      debug: true,
+      entries: ['scripts/main.js'],
+    }).bundle()
+    .pipe(source('searhcer.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({
+      loadMaps: true,
+    }))
+    //    .pipe(uglify())
+    .pipe(sourcemaps.write())
+    .pipe(rename('searcher.min.js'))
+    .pipe(gulp.dest('./dist/'));
+  });
+
+gulp.task('sass', function () {
+  gulp.src('./styles/*.scss')
+  .pipe(plumber())
+  .pipe(sass())
+  .pipe(concat('style.css'))
+  .pipe(minifyCss())
+  .pipe(rename('style.min.css'))
+  .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./styles/**/*.scss', ['sass']);
+  gulp.watch('./scripts/**/*', ['browserify']);
+});
+
+gulp.task('build', ['browserify', 'sass'], function() {
+  gulp.src('index.html')
+  .pipe(gulp.dest('./dist/'));
+});
+
---
+++ scripts/data.js
@@ -0,0 +1,40 @@
+﻿module.exports = {
+  records: [
+    {
+      date: '2011/11/03',
+      id: '1',
+      record: '_imga6s5I0A6Q6Aiu3K8QgQhEawhS2E',
+      tags: [
+        'momoken',
+        'だぁ積み',
+        '後折',
+        'AAABBB',
+      ],
+    },
+
+    {
+      date: '2011/11/03',
+      id: '2',
+      record: '_kgoccchCekkCiS0AcS4kocms0aaMhEiq',
+      tags: [
+        'momoken',
+        '座布団L字',
+        '後折',
+        'ABAACC',
+      ],
+    },
+
+    {
+      date: '2011/11/03',
+      id: '3',
+      record: '_lk6ceCcG2woq5mkQ5c5keQ6A4Elm2ElI',
+      tags: [
+        'momoken',
+        '座布団',
+        '後折',
+        'ABAABC',
+      ],
+    },
+  ],
+};
+
