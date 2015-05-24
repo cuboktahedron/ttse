b8f7e705fa4cdd23bb9e6eb2f3c5efbb01beefba
cuboktahedron <cuboktahedron@gmail.com>
2015-05-24 10:44:01 +0900

    search.jsのテストを作成する

---
+++ scripts/test/search-test.js
@@ -0,0 +1,79 @@
+describe("Word", function() {
+  'use strict';
+
+  var searcher = require('../search');
+
+  var records = [
+    {
+      id: '1',
+      record: 'AAA',
+      tags: ['hoge', 'piyo'],
+    },
+
+    {
+      id: '2',
+      record: 'BBB',
+      tags: [],
+    },
+
+    {
+      id: '3',
+      record: 'CCC',
+      tags: ['123hoge', 'piyo123'],
+    },
+
+    {
+      id: '4',
+      record: 'DDD',
+      tags: ['hoge', 'ほげ'],
+    },
+  ];
+
+  var ids = function(rs) {
+    return rs.map(function(v) { return v.id;
+    });
+  };
+
+  beforeEach(function() {
+  });
+
+  afterEach(function() {
+  });
+
+  it("should matches all records", function() {
+    expect(4).toEqual(searcher(records, '').length);
+  });
+
+  it("should matches perfectly with", function() {
+    expect(['1', '4']).toEqual(ids(searcher(records, 'hoge')));
+  });
+
+  it("should not matches perfectly with", function() {
+    expect(['1']).not.toEqual(ids(searcher(records, 'hoger')));
+  });
+
+  it("should matches that begins with", function() {
+    expect(['1', '3']).toEqual(ids(searcher(records, 'piyo*')));
+  });
+
+  it("should matches that ends with", function() {
+    expect(['1', '3', '4']).toEqual(ids(searcher(records, '*hoge')));
+  });
+
+  it("should matches partialy with", function() {
+    expect(['1', '3', '4']).toEqual(ids(searcher(records, '*oge*')));
+  });
+
+  it("should excludes matching records", function() {
+    expect(['2']).toEqual(ids(searcher(records, '-*hoge*')));
+  });
+
+  it("should ignored case", function() {
+    expect(['1', '4']).toEqual(ids(searcher(records, 'HoGe')));
+  });
+
+  it("should multi byte characters", function() {
+    expect(['4']).toEqual(ids(searcher(records, 'ほげ')));
+  });
+});
+
