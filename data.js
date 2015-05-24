9cd3f30943369592b6cc5eb6a63ae4dbd27931f6
cuboktahedron <cuboktahedron@gmail.com>
2015-05-24 17:04:04 +0900

    data.jsの内容をダミーに変更する

--- data.js
+++ data.js
@@ -1,38 +1,20 @@
 ﻿var records = [
   {
-    date: '2011/11/03',
     id: '1',
-    record: '_imga6s5I0A6Q6Aiu3K8QgQhEawhS2E',
-    tags: [
-      'momoken',
-      'だぁ積み',
-      '後折',
-      'AAABBB',
-    ],
+    record: '',
+    tags: ['AAA'],
   },
 
   {
-    date: '2011/11/03',
     id: '2',
-    record: '_kgoccchCekkCiS0AcS4kocms0aaMhEiq',
-    tags: [
-      'momoken',
-      '座布団L字',
-      '後折',
-      'ABAACC',
-    ],
+    record: '',
+    tags: ['BBB'],
   },
 
   {
-    date: '2011/11/03',
     id: '3',
-    record: '_lk6ceCcG2woq5mkQ5c5keQ6A4Elm2ElI',
-    tags: [
-      'momoken',
-      '座布団',
-      '後折',
-      'ABAABC',
-    ],
+    record: '',
+    tags: ['ABC'],
   },
 ];
 
