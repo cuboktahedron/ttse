82110b48b9a701184434d3a1b445e8a9eea98b68
cuboktahedron <cuboktahedron@gmail.com>
2018-01-28 15:36:41 +0900

    サンプルデータを追加する

--- data.js
+++ data.js
@@ -1,20 +1,20 @@
 ﻿var records = [
   {
     id: '1',
-    record: '',
-    tags: ['AAA'],
+    record: 'ns=QbLl&ss=36gl176401kcwSw000&m=1&v=0.97',
+    tags: ['TSD', 'TOILSZJ'],
   },
 
   {
     id: '2',
-    record: '',
-    tags: ['BBB'],
+    record: 'ns=bKYkHf&ss=1w5Yj174w36Ao35glw1g0000000&m=1&v=0.97',
+    tags: ['パフェ', 'ILSTZOJ'],
   },
 
   {
     id: '3',
-    record: '',
-    tags: ['ABC'],
+    record: 'ns=dvyOxHTsKh&ss=1tcl35gh01A5gNAchM1Dco1hcn01D5sm1g0025Wp1H000&m=1&v=0.97',
+    tags: ['DT砲', 'ISLZOJT'],
   },
 ];
 
